973fb87404674cad02e2c145f6c13e4baf4721a5
THEALIHAKER1 <alifhaker1@gmail.com>
2023-05-21 19:12:52 +0800

    fix: .env not include in .gitignore

--- src/app/(Portfolio)/page.tsx
+++ src/app/(Portfolio)/page.tsx
@@ -1,7 +1,7 @@
 import config from '@/Lib/config';
 
 export const getServerSideProps = async () => {
-  const string: string = config.NEXT_PUBLIC_PORT;
+  const string = config.NEXT_PUBLIC_PORT;
   return {
     props: {
       string,
