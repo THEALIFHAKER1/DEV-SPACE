0042e2d3b367bd67e5e6fb7449e141981eb9ec38
THEALIHAKER1 <alifhaker1@gmail.com>
2023-05-21 19:06:42 +0800

    feat: implement storybook baby

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
       mobile: '576px',
       tablet: '960px',
       desktop: '1440px',
+      xs: '375px',
+      sm: '600px',
+      md: '900px',
+      lg: '1200px',
+      xl: '1536px',
     },
     // fontSize: {
     //   sm: '0.8rem',
