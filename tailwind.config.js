bc9b64d4b795eca5105cc2332bedcc0a8aae5dd1
THEALIHAKER1 <alifhaker1@gmail.com>
2023-05-22 06:15:43 +0800

    fix: fix components direction

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/Global/Components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
