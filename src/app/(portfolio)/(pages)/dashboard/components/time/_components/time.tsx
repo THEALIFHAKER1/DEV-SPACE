3dcce9e674eb54bd1180cb63a60fa6f3fab0c6a0
THEALIFHAKER1 <alifhaker1@gmail.com>
2024-01-29 04:32:58 +0800

    Update Card component styles in time.tsx

--- src/app/(portfolio)/(pages)/dashboard/components/time/_components/time.tsx
+++ src/app/(portfolio)/(pages)/dashboard/components/time/_components/time.tsx
@@ -78,7 +78,7 @@ function NightCard({ time }: { time: Date }) {
 
 function DayCard({ time }: { time: Date }) {
   return (
-    <Card className="relative overflow-hidden h-full hovers w-full md:w-[150px] md:h-[150px] bg-blue-400 dark:bg-neutral-800 text-white md:max-w-[400px] border border-black dark:border-neutral-700">
+    <Card className="relative overflow-hidden  hovers w-full md:w-[150px] h-[150px] bg-blue-400 dark:bg-neutral-800 text-white md:max-w-[400px] border border-black dark:border-neutral-700">
       <motion.div
         aria-hidden
         animate={{
